0667845ef896d8cc6cdc57c66a430b5789faf20f
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2024-07-07 19:13:13 +0600

    create admin protected route

--- app/profile/Profile.tsx
+++ app/profile/Profile.tsx
@@ -5,6 +5,7 @@ import SideBarProfile from "./sideBarProfile";
 import { useLogOutQuery } from "@/redux/features/auth/authApi";
 import { signOut } from "next-auth/react";
 import ProfileInfo from "./ProfileInfo";
+import ChangePassword from "./ChangePassword";
 
 type Props = {
   user: any;
@@ -51,6 +52,11 @@ const Profile: FC<Props> = ({ user }) => {
       {active === 1 && (
         <div className="w-full h-full bg-transparent mt-[80px]">
             <ProfileInfo avatar={avatar} user={user}></ProfileInfo>
+        </div>
+      )}
+      {active === 2 && (
+        <div className="w-full h-full bg-transparent mt-[80px]">
+           <ChangePassword></ChangePassword>
         </div>
       )}
     </div>
