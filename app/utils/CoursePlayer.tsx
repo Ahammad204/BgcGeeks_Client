3beb9a0b4d1456e2863244444d757baf68b81f70
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2024-08-14 01:34:47 +0600

    create course preview

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -44,3 +44,40 @@ const CoursePlayer: FC<Props> = ({ videoUrl }) => {
 };
 
 export default CoursePlayer;
+// import React, { FC } from "react";
+
+// type Props = {
+//   videoUrl: string; // The complete YouTube URL or just the video ID
+//   title: string;
+// };
+
+// const CoursePlayer: FC<Props> = ({ videoUrl,title }) => {
+//   // Function to extract YouTube video ID from a full YouTube URL
+//   const getYouTubeVideoId = (url: string) => {
+//     const urlParts = url.split("v=");
+//     return urlParts.length > 1 ? urlParts[1].split("&")[0] : url;
+//   };
+
+//   const videoId = getYouTubeVideoId(videoUrl);
+
+//   return (
+//     <div style={{ paddingTop: "41%", position: "relative" }}>
+//       <iframe
+//         src={`https://www.youtube.com/embed/${videoId}`}
+//         style={{
+//           border: 0,
+//           width: "90%",
+//           height: "100%",
+//           position: "absolute",
+//           top: 0,
+//           left: 0,
+//         }}
+//         title={title}
+//         allowFullScreen
+//       ></iframe>
+//     </div>
+//   );
+// };
+
+// export default CoursePlayer;
+
