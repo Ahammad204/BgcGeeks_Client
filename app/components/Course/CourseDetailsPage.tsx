f7bc1b2d30947e1256947ff2cb0de9a96919e0cc
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2024-10-12 08:04:18 +0600

    intrigate stripe payment

--- app/components/Course/CourseDetailsPage.tsx
+++ app/components/Course/CourseDetailsPage.tsx
@@ -1,47 +1,89 @@
-import { useGetCourseDetailsQuery } from '@/redux/features/courses/coursesApi';
-import React, { useState } from 'react';
-import Loader from '../Loader/Loader';
-import Heading from '@/app/utils/Heading';
-import Header from '../Header';
-import Footer from '../Footer';
-import CourseDetails from './CourseDetails';
+import { useGetCourseDetailsQuery } from "@/redux/features/courses/coursesApi";
+import React, { useEffect, useState } from "react";
+import Loader from "../Loader/Loader";
+import Heading from "@/app/utils/Heading";
+import Header from "../Header";
+import Footer from "../Footer";
+import CourseDetails from "./CourseDetails";
+import {
+  useCreatePaymentIntentMutation,
+  useGetStripePublishableKeyQuery,
+} from "@/redux/features/orders/ordersApi";
+import { loadStripe } from "@stripe/stripe-js";
 
 type Props = {
-    id:string;
+  id: string;
 };
 
-const CourseDetailsPage = ({id}: Props) => {
+const CourseDetailsPage = ({ id }: Props) => {
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetCourseDetailsQuery(id);
+  const { data: config } = useGetStripePublishableKeyQuery({});
+  const [createPaymentIntent, { data: paymentIntentData }] =
+    useCreatePaymentIntentMutation();
+  const [stripePromise, setStripePromise] = useState<any>(null);
+  const [clientSecret, setClientSecret] = useState("");
 
-    const [route,setRoute] = useState("Login");
-    const [open,setOpen] = useState(false);
-    const {data,isLoading} = useGetCourseDetailsQuery(id);
+  useEffect(() => {
+    if (config) {
+      const publishablekey = config?.publishableKey;
+      setStripePromise(loadStripe(publishablekey));
+    }
+    if (data) {
+      const amount = Math.round(data.course.price * 100);
+      const description = `${data.course.name}`;
+          // Add shipping details here
+          const shipping = {
+            name: 'Jenny Rosen',
+            address: {
+              line1: '510 Townsend St',
+              postal_code: '98140',
+              city: 'San Francisco',
+              state: 'CA',
+              country: 'US',
+            },
+          };
+    
+          createPaymentIntent({ amount, description, shipping });
+    }
+  }, [config, createPaymentIntent, data]);
+
+  
+  useEffect(() => {
+    if (paymentIntentData) {
+      setClientSecret(paymentIntentData?.client_secret);
+    }
+  }, [paymentIntentData]);
 
   return (
     <>
-        {
-            isLoading ? (
-                <Loader/>
-            ):(
-                <div>
-                    <Heading
-                    title={data.course.name + " - BGC Geeks"}
-                    description="BGC GEEKS is platform for students to learn and get help from teachers"
-                    keywords={data?.course?.tags}
-                    />
-                    <Header
-                    route={route}
-                    setRoute={setRoute}
-                    open={open}
-                    setOpen={setOpen}
-                    activeItem={1}
-                    />
-                    <CourseDetails
-                    data={data.course}
-                    />
-                    <Footer/>
-                </div>
-            )
-        }
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Heading
+            title={data.course.name + " - BGC Geeks"}
+            description="BGC GEEKS is platform for students to learn and get help from teachers"
+            keywords={data?.course?.tags}
+          />
+          <Header
+            route={route}
+            setRoute={setRoute}
+            open={open}
+            setOpen={setOpen}
+            activeItem={1}
+          />
+          {stripePromise && (
+            <CourseDetails
+              data={data.course}
+              stripePromise={stripePromise}
+              clientSecret={clientSecret}
+            />
+          )}
+          <Footer />
+        </div>
+      )}
     </>
   );
 };
--- redux/features/orders/ordersApi.ts
+++ redux/features/orders/ordersApi.ts
@@ -9,7 +9,60 @@ export const ordersApi = apiSlice.injectEndpoints({
         credentials: "include" as const,
       }),
     }),
+
+    getStripePublishableKey: builder.query({
+      query: () => ({
+        url: `payment/stripepublishablekey`,
+        method: "GET",
+        credentials: "include" as const,
+      }),
+    }),
+
+    // createPaymentIntent: builder.mutation({
+    //   query: ({amount,description}) => ({
+    //     url: "payment",
+    //     method: "POST",
+    //     body: {
+    //       amount,
+    //       description
+    //     },
+    //     credentials: "include" as const,
+    //   }),
+    // }),
+    createPaymentIntent: builder.mutation({
+      query: ({ amount, description, shipping }) => ({
+        url: "payment", 
+        method: "POST",
+        body: {
+          amount,      
+          description, 
+          shipping
+        },
+        credentials: "include",  
+        headers: {
+          "Content-Type": "application/json",  
+        },
+      }),
+    }),
+    
+    createOrder: builder.mutation({
+      query: ({ courseId, payment_info}) => ({
+        url: "create-order",
+        body: {
+          courseId,
+          payment_info,
+          
+        },
+        method: "POST",
+        credentials: "include" as const,
+      }),
+    }),
   }),
 });
 
-export const {useGetAllOrdersQuery} = ordersApi;
+export const {
+  useGetAllOrdersQuery,
+  useGetStripePublishableKeyQuery,
+  useCreatePaymentIntentMutation,
+  useCreateOrderMutation,
+} = ordersApi;
